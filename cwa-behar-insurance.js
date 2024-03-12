7bb781fcd28ea663e6d19ac25f81542df510a6b7
CWAdevelopers <163160329+CWAdevelopers@users.noreply.github.com>
2024-03-12 13:34:24 -0400

    Add files via upload

---
+++ cwa-behar-insurance.js
@@ -0,0 +1,7 @@
+// proxy.js
+document.addEventListener("DOMContentLoaded", function() {
+  var script = document.createElement('script');
+  script.src = 'https://app.aminos.ai/js/chat_plugin.js';
+  script.setAttribute('data-bot-id', '15795'); // Set the data-bot-id attribute
+  document.head.appendChild(script);
+});
