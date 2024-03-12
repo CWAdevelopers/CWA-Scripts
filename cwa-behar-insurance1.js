65ae32f3145f370a9347d04e7424a6d5ccd8dfce
CWAdevelopers <163160329+CWAdevelopers@users.noreply.github.com>
2024-03-12 14:03:34 -0400

    Delete cwa-behar-insurance1.js

--- cwa-behar-insurance1.js
+++
@@ -1,7 +0,0 @@
-// cwa-behar-insurance1.js
-document.addEventListener("DOMContentLoaded", function() {
-  var script = document.createElement('script');
-  script.src = 'https://app.aminos.ai/js/chat_plugin.js';
-  script.setAttribute('data-bot-id', '15795'); // Set the data-bot-id attribute
-  document.head.appendChild(script);
-});
