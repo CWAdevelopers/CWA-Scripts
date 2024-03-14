3429878096ca3ac96de5a38f7a28531d978270aa
CWAdevelopers <163160329+CWAdevelopers@users.noreply.github.com>
2024-03-14 12:38:53 -0400

    Update behar-insurance2.js

--- behar-insurance2.js
+++ behar-insurance2.js
@@ -1,6 +1,1 @@
-var script = document.createElement('script');
-var botid = document.currentScript.getAttribute("data-bot-id");
-script.src = "https://app.aminos.ai/js/chat_plugin.js";
-script.async = true;
-script.setAttribute('data-bot-id', botid);
-document.head.appendChild(script);
+<script src="https://app.aminos.ai/js/chat_plugin.js" data-bot-id="15795"></script>
